d6ddc9c201340459e3cfa562467e347cab20254f
uga-rosa <uga6603@gmail.com>
2025-04-29 21:11:18 +0900

    EditorFactoryListener

---
+++ src/main/kotlin/com/ugarosa/neovim/listener/NeovimEditorFactoryListener.kt
@@ -0,0 +1,56 @@
+package com.ugarosa.neovim.listener
+
+import com.intellij.openapi.editor.event.EditorFactoryEvent
+import com.intellij.openapi.editor.event.EditorFactoryListener
+import com.ugarosa.neovim.session.NEOVIM_SESSION_KEY
+import java.awt.event.KeyAdapter
+import java.awt.event.KeyEvent
+
+class NeovimEditorFactoryListener : EditorFactoryListener {
+    override fun editorCreated(event: EditorFactoryEvent) {
+        val editor = event.editor
+        editor.contentComponent.addKeyListener(
+            object : KeyAdapter() {
+                override fun keyPressed(e: KeyEvent) {
+                    val session =
+                        editor.getUserData(NEOVIM_SESSION_KEY)
+                            ?: error("NeovimEditorSession does not exist")
+
+                    val key = neovimNotation(e)
+                    println("key = $key")
+                    if (key.length <= 1) {
+                        super.keyPressed(e)
+                    } else {
+                        session.sendInput(key)
+                        e.consume()
+                    }
+                }
+            },
+        )
+    }
+
+    private fun neovimNotation(e: KeyEvent): String {
+        when (e.keyCode) {
+            KeyEvent.VK_CONTROL,
+            KeyEvent.VK_SHIFT,
+            KeyEvent.VK_ALT,
+            KeyEvent.VK_META -> return ""
+        }
+
+        val modifiers =
+            buildList {
+                if (e.isControlDown) add("C")
+                if (e.isShiftDown) add("S")
+                if (e.isAltDown) add("A")
+            }
+        val key = KeyEvent.getKeyText(e.keyCode).lowercase()
+
+        return if (modifiers.isNotEmpty()) {
+            "<${modifiers.joinToString("-")}-$key>"
+        } else if (key.length == 1) {
+            key
+        } else {
+            "<$key>"
+        }
+    }
+}
