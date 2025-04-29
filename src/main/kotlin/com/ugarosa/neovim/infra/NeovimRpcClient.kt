2b03e1f35bb1381ce9a53987b07144e35e5b6f54
uga-rosa <uga6603@gmail.com>
2025-04-29 20:35:45 +0900

    NeovimRpcClient

--- src/main/kotlin/com/ugarosa/neovim/infra/NeovimRpcClient.kt
+++ src/main/kotlin/com/ugarosa/neovim/infra/NeovimRpcClient.kt
@@ -1,5 +1,12 @@
 package com.ugarosa.neovim.infra
 
+import com.ugarosa.neovim.service.BufferId
+import com.ugarosa.neovim.service.TabPageId
+import com.ugarosa.neovim.service.WindowId
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
 import org.msgpack.core.MessagePack
 import org.msgpack.value.Value
 import java.io.InputStream
@@ -13,11 +20,47 @@ class NeovimRpcClient(
     private val messageIdGenerator = AtomicInteger(0)
     private val packer = MessagePack.newDefaultPacker(output)
     private val unpacker = MessagePack.newDefaultUnpacker(input)
+    private val responseChannel = Channel<Response>(Channel.UNLIMITED)
 
-    fun sendRequest(
+    private val pushHandlers = mutableListOf<(PushNotification) -> Unit>()
+
+    fun registerPushHandler(handler: (PushNotification) -> Unit) {
+        pushHandlers.add(handler)
+    }
+
+    init {
+        CoroutineScope(Dispatchers.IO).launch {
+            while (true) {
+                unpacker.unpackArrayHeader()
+                val type = unpacker.unpackInt()
+                when (type) {
+                    1 -> {
+                        val msgId = unpacker.unpackInt()
+                        val error = unpacker.unpackValue()
+                        val result = unpacker.unpackValue()
+                        responseChannel.send(Response(msgId, error, result))
+                    }
+
+                    2 -> {
+                        val method = unpacker.unpackString()
+                        val params = unpacker.unpackValue()
+                        val notification = PushNotification(method, params)
+                        pushHandlers.forEach { it(notification) }
+                    }
+
+                    else -> {
+                        error("Unknown message type: $type")
+                    }
+                }
+            }
+        }
+    }
+
+    fun requestSync(
         method: String,
         params: List<Any?> = emptyList(),
-    ): Int {
+        timeoutMillis: Int? = 500,
+    ): Response {
         val msgId = messageIdGenerator.getAndIncrement()
 
         packer.packArrayHeader(4)
@@ -27,19 +70,23 @@ class NeovimRpcClient(
         packParams(params)
         packer.flush()
 
-        return msgId
-    }
+        val deadline = timeoutMillis?.let { System.currentTimeMillis() + it }
 
-    fun receiveResponse(): Response {
-        unpacker.unpackArrayHeader() // should be 4
-        val type = unpacker.unpackInt()
-        check(type == 1) { "Expected response (1), but got $type" }
-
-        val msgId = unpacker.unpackInt()
-        val error = unpacker.unpackValue()
-        val result = unpacker.unpackValue()
-
-        return Response(msgId, error, result)
+        while (true) {
+            val result = responseChannel.tryReceive()
+            if (result.isSuccess) {
+                val response = result.getOrThrow()
+                if (response.msgId == msgId) {
+                    return response
+                } else {
+                    // TODO: Handle unexpected message
+                }
+            }
+            if (deadline != null && System.currentTimeMillis() > deadline) {
+                error("Timeout waiting for response to $method")
+            }
+            Thread.sleep(1)
+        }
     }
 
     private fun packParam(param: Any?) {
@@ -52,7 +99,20 @@ class NeovimRpcClient(
                 param.forEach { packParam(it) }
             }
 
+            is Map<*, *> -> {
+                packer.packMapHeader(param.size)
+                param.forEach { (key, value) ->
+                    packParam(key)
+                    packParam(value)
+                }
+            }
+
             null -> packer.packNil()
+
+            is BufferId -> packer.packLong(param.value)
+            is WindowId -> packer.packLong(param.value)
+            is TabPageId -> packer.packLong(param.value)
+
             else -> throw IllegalArgumentException("Unsupported param type: ${param::class}")
         }
     }
@@ -66,5 +126,10 @@ class NeovimRpcClient(
         val msgId: Int,
         val error: Value,
         val result: Value,
+    )
+
+    data class PushNotification(
+        val method: String,
+        val params: Value,
     )
 }
