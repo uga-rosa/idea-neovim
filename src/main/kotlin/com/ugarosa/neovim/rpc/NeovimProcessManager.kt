693558823c118099200a7f97fa4deed0aed6d5bf
uga-rosa <uga6603@gmail.com>
2025-05-02 23:35:30 +0900

    process manager

--- src/main/kotlin/com/ugarosa/neovim/rpc/NeovimProcessManager.kt
+++ src/main/kotlin/com/ugarosa/neovim/rpc/NeovimProcessManager.kt
@@ -1,32 +1,68 @@
 package com.ugarosa.neovim.rpc
 
+import com.intellij.openapi.diagnostic.thisLogger
 import java.io.InputStream
 import java.io.OutputStream
 import java.net.Socket
+import java.util.concurrent.TimeUnit
 
-class NeovimProcessManager {
-    private lateinit var process: Process
-    private lateinit var inputStream: InputStream
-    private lateinit var outputStream: OutputStream
+interface NeovimProcessManager {
+    fun getInputStream(): InputStream
+
+    fun getOutputStream(): OutputStream
+
+    fun close()
+}
+
+class NeovimProcessManagerImpl : NeovimProcessManager {
+    private val delegate: NeovimProcessManager =
+        run {
+            val addr = System.getProperty("nvim.listen.address")
+            thisLogger().warn("Neovim address: $addr")
+            if (addr != null) {
+                SocketProcessManager(addr)
+            } else {
+                EmbedProcessManager()
+            }
+        }
+
+    override fun getInputStream() = delegate.getInputStream()
+
+    override fun getOutputStream() = delegate.getOutputStream()
+
+    override fun close() = delegate.close()
+}
+
+class SocketProcessManager(
+    address: String,
+) : NeovimProcessManager {
+    private val socket: Socket
+    private val inputStream: InputStream
+    private val outputStream: OutputStream
 
     init {
-        val debugAddr = System.getenv("NVIM_LISTEN_ADDRESS")
-        if (debugAddr != null) {
-            connectToExistingProcess(debugAddr)
-        } else {
-            startNewProcess()
-        }
-    }
-
-    private fun connectToExistingProcess(debugAddress: String) {
-        val (host, portStr) = debugAddress.split(":")
+        val (host, portStr) = address.split(":")
         val port = portStr.toInt()
-        val socket = Socket(host, port)
+        socket = Socket(host, port)
         inputStream = socket.getInputStream()
         outputStream = socket.getOutputStream()
     }
 
-    private fun startNewProcess() {
+    override fun getInputStream() = inputStream
+
+    override fun getOutputStream() = outputStream
+
+    override fun close() {
+        socket.close()
+    }
+}
+
+class EmbedProcessManager : NeovimProcessManager {
+    private val process: Process
+    private val inputStream: InputStream
+    private val outputStream: OutputStream
+
+    init {
         val builder =
             ProcessBuilder(
                 "nvim",
@@ -39,7 +75,18 @@ class NeovimProcessManager {
         outputStream = process.outputStream
     }
 
-    fun getInputStream(): InputStream = inputStream
+    override fun getInputStream() = inputStream
 
-    fun getOutputStream(): OutputStream = outputStream
+    override fun getOutputStream() = outputStream
+
+    override fun close() {
+        runCatching { inputStream.close() }
+        runCatching { outputStream.close() }
+        runCatching { process.destroy() }
+        runCatching {
+            if (!process.waitFor(500, TimeUnit.MILLISECONDS)) {
+                process.destroyForcibly()
+            }
+        }
+    }
 }
