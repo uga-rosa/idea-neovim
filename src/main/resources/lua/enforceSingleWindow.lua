54afc9d227f0f1309ebaddb719c7a0faffe5a755
uga-rosa <uga6603@gmail.com>
2025-05-04 15:28:10 +0900

    fix window check

--- src/main/resources/lua/enforceSingleWindow.lua
+++ src/main/resources/lua/enforceSingleWindow.lua
@@ -1,9 +1,11 @@
+local winId = vim.api.nvim_get_current_win()
 vim.api.nvim_create_autocmd("WinNew", {
     group = vim.api.nvim_create_augroup("IdeaNeovim:EnforceSingleWindow", { clear = true }),
     callback = vim.schedule_wrap(function()
-        local current = vim.api.nvim_get_current_win()
-        if #vim.api.nvim_list_wins() > 1 then
-            vim.api.nvim_win_close(current, true)
+        for _, win in ipairs(vim.api.nvim_list_wins()) do
+            if win ~= winId then
+                vim.api.nvim_win_close(win, true)
+            end
         end
     end)
 })
