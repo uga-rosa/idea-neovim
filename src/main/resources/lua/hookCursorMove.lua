3f2ae712548e363aadde440680e523a5a4fef02f
uga-rosa <uga6603@gmail.com>
2025-05-05 12:44:31 +0900

    fire nvim_cursor_move event on BufEnter

--- src/main/resources/lua/hookCursorMove.lua
+++ src/main/resources/lua/hookCursorMove.lua
@@ -1,13 +1,25 @@
 local chanId = ...
+
+local group = vim.api.nvim_create_augroup("IdeaNeovim:CursorMove", { clear = true })
 vim.g.__idea_neovim_cursor_move_ignore_count = 0
 
 vim.api.nvim_create_autocmd("CursorMoved", {
-    group = vim.api.nvim_create_augroup("IdeaNeovim:CursorMove", { clear = true }),
+    group = group,
     callback = function()
         if vim.g.__idea_neovim_cursor_move_ignore_count > 0 then
             vim.g.__idea_neovim_cursor_move_ignore_count = vim.g.__idea_neovim_cursor_move_ignore_count - 1
             return
         end
+        local bufferId = vim.api.nvim_get_current_buf()
+        local cursor = vim.api.nvim_win_get_cursor(0)
+        -- [bufferId, line, column]
+        vim.rpcnotify(chanId, "nvim_cursor_move", bufferId, cursor[1], cursor[2])
+    end,
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    group = group,
+    callback = function()
         local bufferId = vim.api.nvim_get_current_buf()
         local cursor = vim.api.nvim_win_get_cursor(0)
         -- [bufferId, line, column]
