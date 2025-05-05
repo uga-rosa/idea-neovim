787ae0acab055237f8c22ae4921e53c9a32f52cc
uga-rosa <uga6603@gmail.com>
2025-05-06 07:13:23 +0900

    send nvim_cursor_move_event when entering insert mode

--- src/main/resources/lua/hookModeChange.lua
+++ src/main/resources/lua/hookModeChange.lua
@@ -6,5 +6,11 @@ vim.api.nvim_create_autocmd("ModeChanged", {
 		local bufferId = vim.api.nvim_get_current_buf()
 		-- [bufferId, mode]
 		vim.rpcnotify(chanId, "nvim_mode_change_event", bufferId, vim.v.event.new_mode)
+		-- If the new mode is insert mode, we need to send the cursor position
+		-- since CursorMoved event will not be triggered.
+		if vim.startswith(vim.v.event.new_mode, "i") then
+			local cursor = vim.api.nvim_win_get_cursor(0)
+			vim.rpcnotify(chanId, "nvim_cursor_move_event", bufferId, cursor[1], cursor[2])
+		end
 	end,
 })
