76f70e0aa3ada24eeafdf7ac561fe990f3699fd3
uga-rosa <uga6603@gmail.com>
2025-05-07 03:19:56 +0900

    interface NeovimModeManager

--- src/main/kotlin/com/ugarosa/neovim/common/ApplicationServiceUtils.kt
+++ src/main/kotlin/com/ugarosa/neovim/common/ApplicationServiceUtils.kt
@@ -4,6 +4,7 @@ import com.intellij.openapi.components.service
 import com.ugarosa.neovim.config.neovim.NeovimOptionManager
 import com.ugarosa.neovim.config.neovim.NeovimOptionManagerImpl
 import com.ugarosa.neovim.mode.NeovimModeManager
+import com.ugarosa.neovim.mode.NeovimModeManagerImpl
 import com.ugarosa.neovim.rpc.client.NeovimRpcClient
 import com.ugarosa.neovim.rpc.client.NeovimRpcClientImpl
 
@@ -11,4 +12,4 @@ fun getClient(): NeovimRpcClient = service<NeovimRpcClientImpl>()
 
 fun getOptionManager(): NeovimOptionManager = service<NeovimOptionManagerImpl>()
 
-fun getModeManager(): NeovimModeManager = service()
+fun getModeManager(): NeovimModeManager = service<NeovimModeManagerImpl>()
---
+++ src/main/kotlin/com/ugarosa/neovim/mode/NeovimModeManagerImpl.kt
@@ -0,0 +1,27 @@
+package com.ugarosa.neovim.mode
+
+import com.intellij.openapi.components.Service
+import com.ugarosa.neovim.rpc.event.NeovimMode
+import java.util.concurrent.atomic.AtomicReference
+
+@Service(Service.Level.APP)
+class NeovimModeManagerImpl : NeovimModeManager {
+    private val mode = AtomicReference(NeovimMode.default)
+
+    override fun getMode(): NeovimMode {
+        return mode.get()
+    }
+
+    // Returns true if the value was different and was updated
+    override fun setMode(newMode: NeovimMode): Boolean {
+        val old =
+            mode.getAndUpdate { cur ->
+                if (cur == newMode) {
+                    cur
+                } else {
+                    newMode
+                }
+            }
+        return old != newMode
+    }
+}
