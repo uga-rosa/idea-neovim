d838b54b8d543e37be15d29311158689421eb480
tsuyoshi.nakai <tsuyoshi.nakai@dena.com>
2025-05-08 15:39:44 +0900

    stylua

--- src/main/resources/lua/setCursorWithoutEvent.lua
+++ src/main/resources/lua/setCursorWithoutEvent.lua
@@ -4,13 +4,13 @@ local cur = vim.api.nvim_win_get_cursor(0)
 local cur_row, cur_col = cur[1], cur[2]
 
 if target_row == cur_row and target_col == cur_col then
-    -- No need to move the cursor
-    return
+	-- No need to move the cursor
+	return
 end
 
 local mode = vim.api.nvim_get_mode().mode
 if not (vim.startswith(mode, "i") or vim.startswith(mode, "c")) then
-    vim.g.__idea_neovim_cursor_move_ignore_count = vim.g.__idea_neovim_cursor_move_ignore_count + 1
+	vim.g.__idea_neovim_cursor_move_ignore_count = vim.g.__idea_neovim_cursor_move_ignore_count + 1
 end
 
 vim.api.nvim_win_set_cursor(0, { ... })
