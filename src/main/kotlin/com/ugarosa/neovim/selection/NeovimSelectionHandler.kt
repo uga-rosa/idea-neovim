5c5cbd5affa0dda9eba0f2b48449ce6aa402e6a1
uga-rosa <uga6603@gmail.com>
2025-05-10 17:14:54 +0900

    remove unnecessary mutex

--- src/main/kotlin/com/ugarosa/neovim/selection/NeovimSelectionHandler.kt
+++ src/main/kotlin/com/ugarosa/neovim/selection/NeovimSelectionHandler.kt
@@ -11,8 +11,6 @@ import com.intellij.openapi.editor.markup.RangeHighlighter
 import com.intellij.openapi.editor.markup.TextAttributes
 import com.ugarosa.neovim.rpc.event.VisualSelectionEvent
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.withContext
 
 class NeovimSelectionHandler(
@@ -26,12 +24,9 @@ class NeovimSelectionHandler(
             backgroundColor = selectionColor
         }
     private val highlighters = mutableListOf<RangeHighlighter>()
-    private val mutex = Mutex()
 
     suspend fun applyVisualSelectionEvent(event: VisualSelectionEvent) {
         logger.trace("applyVisualSelectionEvent: $event")
-
-        resetSelection()
 
         val offsets =
             event.regions.map { region ->
@@ -43,30 +38,28 @@ class NeovimSelectionHandler(
         logger.trace("Offsets: $offsets")
 
         withContext(Dispatchers.EDT) {
-            mutex.withLock {
-                offsets.forEach { (startOffset, endOffset) ->
-                    val highlighter =
-                        editor.markupModel.addRangeHighlighter(
-                            startOffset,
-                            endOffset,
-                            HighlighterLayer.SELECTION,
-                            attributes,
-                            HighlighterTargetArea.EXACT_RANGE,
-                        )
-                    highlighters.add(highlighter)
-                }
+            resetSelection()
+
+            offsets.forEach { (startOffset, endOffset) ->
+                val highlighter =
+                    editor.markupModel.addRangeHighlighter(
+                        startOffset,
+                        endOffset,
+                        HighlighterLayer.SELECTION,
+                        attributes,
+                        HighlighterTargetArea.EXACT_RANGE,
+                    )
+                highlighters.add(highlighter)
             }
         }
     }
 
     suspend fun resetSelection() {
         withContext(Dispatchers.EDT) {
-            mutex.withLock {
-                highlighters.forEach {
-                    editor.markupModel.removeHighlighter(it)
-                }
-                highlighters.clear()
+            highlighters.forEach {
+                editor.markupModel.removeHighlighter(it)
             }
+            highlighters.clear()
         }
     }
 }
