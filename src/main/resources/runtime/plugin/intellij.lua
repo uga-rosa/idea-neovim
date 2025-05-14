db3effec70e76dee1a35d350b802184e050bd862
tsuyoshi.nakai <tsuyoshi.nakai@dena.com>
2025-05-14 16:09:50 +0900

    fix typo

--- src/main/resources/runtime/plugin/intellij.lua
+++ src/main/resources/runtime/plugin/intellij.lua
@@ -2,8 +2,8 @@ local win_id = vim.api.nvim_get_current_win()
 
 -- Simplify grid conversion between Neovim and IntelliJ
 vim.api.nvim_set_option_value("wrap", false, { win = win_id })
-vim.opt.nvim_set_option_value("number", false, { win = win_id })
-vim.opt.nvim_set_option_value("signcolumn", "no", { win = win_id })
+vim.api.nvim_set_option_value("number", false, { win = win_id })
+vim.api.nvim_set_option_value("signcolumn", "no", { win = win_id })
 
 -- Enforce single window
 vim.api.nvim_create_autocmd("WinNew", {
