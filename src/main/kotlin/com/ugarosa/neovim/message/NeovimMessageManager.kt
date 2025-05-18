ed600e9cb4ffc080bace501aa9046dac5a7fa964
uga-rosa <uga6603@gmail.com>
2025-05-18 23:50:45 +0900

    add badge to tool window icon

--- src/main/kotlin/com/ugarosa/neovim/message/NeovimMessageManager.kt
+++ src/main/kotlin/com/ugarosa/neovim/message/NeovimMessageManager.kt
@@ -1,22 +1,45 @@
 package com.ugarosa.neovim.message
 
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.EDT
+import com.intellij.openapi.application.invokeLater
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowManager
+import com.intellij.openapi.wm.ex.ToolWindowManagerListener
 import com.ugarosa.neovim.common.focusProject
+import com.ugarosa.neovim.logger.myLogger
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 @Service(Service.Level.APP)
 class NeovimMessageManager {
+    private val logger = myLogger()
+
+    init {
+        // Reset icon when tool window is shown
+        ApplicationManager.getApplication().messageBus.connect().subscribe(
+            ToolWindowManagerListener.TOPIC,
+            object : ToolWindowManagerListener {
+                override fun toolWindowShown(toolWindow: ToolWindow) {
+                    if (toolWindow.id != NeovimMessageToolWindowFactory.WINDOW_ID) return
+                    invokeLater {
+                        toolWindow.setIcon(NeovimMessageIcon.base)
+                    }
+                }
+            },
+        )
+    }
+
     suspend fun handleMessageEvent(event: MessageEvent) =
         withContext(Dispatchers.EDT) {
             val project = focusProject() ?: return@withContext
             val livePane = project.service<MessageLivePane>()
             val historyPane = project.service<MessageHistoryPane>()
+
+            logger.trace("Handling message event: $event")
 
             when (event) {
                 is MessageEvent.Show -> {
@@ -36,14 +59,10 @@ class NeovimMessageManager {
 
                 is MessageEvent.Flush -> {
                     toolWindow(project)?.let { tw ->
-                        if (livePane.isHide()) {
-                            tw.hide()
+                        if (livePane.flush()) {
+                            tw.setIcon(NeovimMessageIcon.withBadge)
                         } else {
-                            livePane.flush()
-                            tw.show {
-                                val live = tw.contentManager.findContent(MessageLivePane.DISPLAY_NAME)
-                                tw.contentManager.setSelectedContent(live)
-                            }
+                            tw.hide()
                         }
                     }
                 }
--- src/main/kotlin/com/ugarosa/neovim/message/NeovimMessageToolWindowFactory.kt
+++ src/main/kotlin/com/ugarosa/neovim/message/NeovimMessageToolWindowFactory.kt
@@ -2,16 +2,26 @@ package com.ugarosa.neovim.message
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.IconLoader
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBScrollPane
 import com.intellij.ui.content.ContentFactory
+import com.ugarosa.neovim.common.OverlayIcon
+
+object NeovimMessageIcon {
+    val base = IconLoader.getIcon("/icons/neovim-message@20x20.svg", javaClass)
+    private val badge = IconLoader.getIcon("/icons/green-circle.svg", javaClass)
+    val withBadge = OverlayIcon(base, badge)
+}
 
 class NeovimMessageToolWindowFactory : ToolWindowFactory {
     override fun createToolWindowContent(
         project: Project,
         toolWindow: ToolWindow,
     ) {
+        toolWindow.setIcon(NeovimMessageIcon.base)
+
         val contentFactory = ContentFactory.getInstance()
 
         val livePane = project.service<MessageLivePane>()
